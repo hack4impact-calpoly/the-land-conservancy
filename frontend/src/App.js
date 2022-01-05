780acfa50069b33b52d9658823a48151a0a7c9b4
Marissa Leal Darnell <mdarnell@calpoly.edu>
2022-01-04 23:03:19 -0800

    test commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ console.log('one');
 var ww = process.env.NODE_ENV.trim.toLowerCase();
 if (ww === 'production') {
   console.log('Another one');     
-} else {
+} else {   
     console.log('test')
 }
 
