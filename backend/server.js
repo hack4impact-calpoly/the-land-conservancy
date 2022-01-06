605862f49ed70fff70ac79e38269a2717e482324
Marissa Leal Darnell <mdarnell@calpoly.edu>
2022-01-05 19:57:49 -0800

    initializing project

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express'); // 1. includes Express
+
+const app = express(); // 2. initializes Express
+
+/*
+ * other code here :)
+ */
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+app.listen(3001); // 3. runs Express
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,5 @@
 import './App.css';
 
-
-console.log('one');
-const ww = process.env.NODE_ENV.trim().toLowerCase();
-if (ww === 'production') {
-  console.log('Another one');     
-} else {   
-    console.log('test')
-}  
-
 function App() {
   return (
     <div className="App">
@@ -16,14 +7,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
