e9cc910a775fc4f6f891e78fbbcb5a7bc1ce282d
Ben McMann <bbmcmann@volosys.com>
2022-02-05 15:30:08 -0800

    fixed linting format

--- frontend/src/components/pages/eventCard.tsx
+++ frontend/src/components/pages/eventCard.tsx
@@ -3,40 +3,34 @@ import styled from 'styled-components';
 import { Card } from '@mui/material';
 
 const StyledCard = styled(Card)`
-    border-radius: 6px;
-    padding: 14px;
-    padding-left: 25px;
+  border-radius: 6px;
+  padding: 14px;
+  padding-left: 25px;
 
-    margin-bottom: 10px;
-    text-align: left;
+  margin-bottom: 10px;
+  text-align: left;
 `;
 
 const EventTitle = styled.p`
-    font-weight: bold;
-    font-size: 14px;
-    color: black;
+  font-weight: bold;
+  font-size: 14px;
+  color: black;
 `;
 
-
 type EventCardProps = {
-    title: string,
-    location: string
-}
+  title: string;
+  location: string;
+};
 
 /*  for sime reason background color fo card would not chnage without inline css    */
 
-export default function EventCard({
-    title,
-    location
-}: EventCardProps) {
-   
-    return (
-        <div>
-            
-            <StyledCard style={{backgroundColor: "#F1F1F1"}}>
-                <EventTitle>{title}</EventTitle>
-                <p>{location}</p>
-            </StyledCard>
-        </div>
-    );
+export default function EventCard({ title, location }: EventCardProps) {
+  return (
+    <div>
+      <StyledCard style={{ backgroundColor: '#F1F1F1' }}>
+        <EventTitle>{title}</EventTitle>
+        <p>{location}</p>
+      </StyledCard>
+    </div>
+  );
 }
--- frontend/src/components/pages/events.tsx
+++ frontend/src/components/pages/events.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import {Container } from '@mui/material';
+import { Container } from '@mui/material';
 import { BiArrowBack } from 'react-icons/bi';
 import EventCard from './eventCard';
 
@@ -12,85 +12,85 @@ const StyledArrow = styled(BiArrowBack)`
 `;
 
 const StyledHeader = styled.p`
-    font-family: Poppins;
-    font-style: normal;
-    font-weight: 500;
-    font-size: 18px;
-    line-height: 27px;
+  font-family: Poppins;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 27px;
 
-    color: #000000;
+  color: #000000;
 `;
 
 const StyledContainer = styled(Container)`
-    border-radius: 7px;
-    font-family: Poppins;
-    margin: 5px;
-    padding: 20px;
-    align-items: left;
-    justify-content: left;
+  border-radius: 7px;
+  font-family: Poppins;
+  margin: 5px;
+  padding: 20px;
+  align-items: left;
+  justify-content: left;
 `;
 
 const StyledButton = styled.button`
-    background: #5f8f3e;
-    color: white;
-    font-family: Poppins;
-    font-size: 15px;
-    text-align: center;
-    border-radius: 6px;
-    border-color: #5f8f3e;
-    border-style: solid;
-    margin-top: 6px;
-    margin-bottom: 10px;
-    width: 100%;
-    height: 36px;
-    align-self: center;
+  background: #5f8f3e;
+  color: white;
+  font-family: Poppins;
+  font-size: 15px;
+  text-align: center;
+  border-radius: 6px;
+  border-color: #5f8f3e;
+  border-style: solid;
+  margin-top: 6px;
+  margin-bottom: 10px;
+  width: 100%;
+  height: 36px;
+  align-self: center;
 `;
 
 const testEvents = [
-    {
-        title: "Ben's party",
-        start: new Date(121170),
-        end: new Date(),
-        location: "35.30254888400675, -120.69751392967409",
-        notes: "very fun",
-        id: 12
-    },
-    {
-        title: "Kyle Concert",
-        start: new Date(22222222),
-        end: new Date(44444444444444),
-        location: "university union",
-        notes: "also very fun",
-        id: 13
-    },
-    {
-        title: "Armstrong's Winter Staff Party",
-        start: new Date(),
-        end: new Date(),
-        location: "PAC",
-        notes: "Everyone's invited!",
-        id: 99900
-    }
-]
+  {
+    title: "Ben's party",
+    start: new Date(121170),
+    end: new Date(),
+    location: '35.30254888400675, -120.69751392967409',
+    notes: 'very fun',
+    id: 12,
+  },
+  {
+    title: 'Kyle Concert',
+    start: new Date(22222222),
+    end: new Date(44444444444444),
+    location: 'university union',
+    notes: 'also very fun',
+    id: 13,
+  },
+  {
+    title: "Armstrong's Winter Staff Party",
+    start: new Date(),
+    end: new Date(),
+    location: 'PAC',
+    notes: "Everyone's invited!",
+    id: 99900,
+  },
+];
 
 export default function Events() {
-    console.log(testEvents)
+  console.log(testEvents);
 
-    const eventCards = testEvents.map(event => {
-        return (
-            <EventCard key={event.id} title={event.title} location={event.location} />    
-        )
-    });
-
-    console.log(eventCards)
+  const eventCards = testEvents.map((event) => {
     return (
-        <div>
-            <StyledContainer maxWidth="sm"> 
-                <StyledArrow />
-                <StyledHeader>Events</StyledHeader>
-                {eventCards}
-                <StyledButton>Select</StyledButton>
-            </StyledContainer>
-        </div>
+      <EventCard key={event.id} title={event.title} location={event.location} />
     );
+  });
+
+  console.log(eventCards);
+  return (
+    <div>
+      <StyledContainer maxWidth="sm">
+        <StyledArrow />
+        <StyledHeader>Events</StyledHeader>
+        {eventCards}
+        <StyledButton>Select</StyledButton>
+      </StyledContainer>
+    </div>
+  );
 }
