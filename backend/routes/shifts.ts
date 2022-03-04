a94ce2c4fc8300b3465600c56e792cebd3622c91
Marissa Leal Darnell <63181280+marissadarnell@users.noreply.github.com>
2022-03-03 17:33:21 -0800

    Revert "Extra endpoints"

--- backend/routes/shifts.ts
+++ backend/routes/shifts.ts
@@ -2,8 +2,6 @@ import Shift from '../models/shiftSchema';
 
 const express = require('express');
 
-const lodash = require('lodash');
-
 const router = express.Router();
 
 // get all shifts
@@ -41,37 +39,6 @@ router.post('/', async (req: any, res: any) => {
     shift = await shift.save();
     console.log(shift);
     res.json(shift);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-
-/* deletes event by id */
-router.delete('/:shiftid', async (req: any, res: any) => {
-  try {
-    const temp = await Shift.findByIdAndDelete(req.params.shiftid);
-    res.send(temp);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-
-/* patches existing event in databse with any
-   information passed through req body */
-router.patch('/:shiftid', async (req: any, res: any) => {
-  try {
-    const updatedShift = req.body;
-    const shiftInDb = await Shift.findById(req.params.shiftid);
-
-    /* lodash used to easily combine the new event
-    json object witht the existing one in the db */
-    let patchedShift = shiftInDb;
-    lodash.forIn(updatedShift, (value: any, key: string) => {
-      patchedShift[key] = value;
-    });
-
-    patchedShift = await patchedShift.save();
-    res.json(patchedShift);
   } catch (error) {
     res.status(400).send(error);
   }
