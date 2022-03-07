f4329b114c56bc7fb348d57e760d827eda7ab1ec
Sam Wathen <sfwathen@calpoly.edu>
2022-03-06 20:43:21 -0800

    removed lodash

--- backend/routes/users.ts
+++ backend/routes/users.ts
@@ -2,8 +2,6 @@ import User from '../models/userSchema';
 
 const express = require('express');
 
-const lodash = require('lodash');
-
 const router = express.Router();
 
 // get all users
@@ -19,7 +17,7 @@ router.get('/', async (req: any, res: any) => {
 // get specific user
 router.get('/:userId', async (req: any, res: any) => {
   try {
-    const temp = await User.findOne({ _id: req.params.userId });
+    const temp = await User.findById(req.params.userId);
     res.send(temp);
   } catch (error) {
     res.status(400).send(error);
@@ -51,25 +49,19 @@ router.post('/', async (req: any, res: any) => {
 });
 
 // patch a user (will replace the fields passed in through the req.body)
-router.patch('/patch/:userid', async (req: any, res: any) => {
+router.patch('/:userId', async (req: any, res: any) => {
   try {
-    const updatedUser = req.body;
-    const userInDb = await User.findById(req.params.userid);
-
-    let patchedUser = userInDb;
-    lodash.forIn(updatedUser, (value: any, key: string) => {
-      patchedUser[key] = value;
-    });
-
-    patchedUser = await patchedUser.save();
-    res.json(patchedUser);
+    const { userId } = req.params;
+    const updates = req.body;
+    const updatedUser = await User.findByIdAndUpdate(userId, updates);
+    res.json(updatedUser);
   } catch (error) {
     res.status(400).send(error);
   }
 });
 
 // delete user by id
-router.delete('/delete/:userId', async (req: any, res: any) => {
+router.delete('/:userId', async (req: any, res: any) => {
   try {
     const temp = await User.deleteOne({ _id: req.params.userId });
     res.send(temp);
