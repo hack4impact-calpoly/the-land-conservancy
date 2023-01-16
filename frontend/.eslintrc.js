772e6bee82705073530cdf84c56db35b8b77d718
ryyhu <ryanhu021@gmail.com>
2023-01-15 16:18:41 -0800

    ci: update eslint config

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -4,39 +4,50 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'airbnb-typescript',
-    'prettier',
+    "plugin:react/recommended",
+    "airbnb",
+    "airbnb-typescript",
+    "prettier",
   ],
   overrides: [
     {
-      files: ['**/*.test.js', '**/*.test.jsx', '**/*.test.ts', '**/*.test.tsx'],
+      files: ["**/*.test.js", "**/*.test.jsx", "**/*.test.ts", "**/*.test.tsx"],
       env: {
         jest: true,
       },
     },
   ],
-  parser: '@typescript-eslint/parser',
+  ignorePatterns: [
+    "**/custom.d.ts",
+    "**/amplify/*",
+    "**/node_modules",
+    "**/build",
+  ],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 'latest',
-    project: './tsconfig.eslint.json',
+    ecmaVersion: "latest",
+    project: "./tsconfig.eslint.json",
     tsconfigRootDir: __dirname,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: ['react', '@typescript-eslint', 'prettier'],
+  plugins: ["react", "@typescript-eslint", "prettier"],
   rules: {
-    'prettier/prettier': 'error',
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
     // suppress errors for missing 'import React' in files
-    'react/react-in-jsx-scope': 'off',
+    "react/react-in-jsx-scope": "off",
     // allow prop-spreading
-    'react/jsx-props-no-spreading': 'off',
-    'react/jsx-filename-extension': 'off',
-    'no-underscore-dangle': 'off',
-    'no-plusplus': 'off',
-    'no-console': 'off',
+    "react/jsx-props-no-spreading": "off",
+    "react/jsx-filename-extension": "off",
+    "no-underscore-dangle": "off",
+    "no-plusplus": "off",
+    "no-console": "off",
   },
 };
