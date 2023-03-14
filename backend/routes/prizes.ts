6694b4871f6b5f38b850ac2cc7e996b03f396d5e
joieng1 <johncieng1@gmail.com>
2023-03-13 17:10:39 -0700

    feat: edit backend to handle img upload to aws

--- backend/routes/prizes.ts
+++ backend/routes/prizes.ts
@@ -1,11 +1,36 @@
 import express from "express";
+import dotenv from "dotenv";
+import multer from "multer";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import Prize from "../models/prizeSchema";
 
 export {};
 
+dotenv.config();
+
 const router = express.Router();
 
 router.use(express.json());
+
+// connect to aws s3
+const bucketName = process.env.BUCKET_NAME;
+const region = process.env.BUCKET_REGION;
+const accessKeyId = "AKIAZHFOZOFY3VPJTJ4A";
+const secretAccessKey = "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";
+
+const s3Client = new S3Client({
+  region,
+  endpoint: `https://s3.${region}.amazonaws.com`,
+  credentials: {
+    accessKeyId,
+    secretAccessKey,
+  },
+});
+
+// set up multer
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 // get all prizes
 router.get("/", async (req: any, res: any) => {
@@ -29,6 +54,36 @@ router.patch("/:id", async (req: any, res: any) => {
     res.json(newPrize);
   } catch (error) {
     res.status(400).send(error);
+  }
+});
+
+// eslint-disable-next-line consistent-return
+router.post("/upload", upload.single("file"), async (req: any, res: any) => {
+  const { file } = req;
+  // return error if file not found
+  if (file === null) {
+    return res.status(400).send("No file uploaded.");
+  }
+
+  const fileName =
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15);
+
+  const params = {
+    Bucket: bucketName,
+    Body: file.buffer,
+    Key: fileName,
+    ContentType: file.mimetype,
+  };
+  try {
+    // put object into s3 bucket
+    await s3Client.send(new PutObjectCommand(params));
+    // returns key of the image
+    const url = `https://land-conservancy.s3.us-west-1.amazonaws.com/${fileName}`;
+    // return url
+    res.send(url);
+  } catch (error) {
+    res.send(error);
   }
 });
 
